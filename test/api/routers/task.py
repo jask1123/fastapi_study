7cff3c73f8ef9fe7aa5c3dba0cca330daea89afd
jask1123 <50744263+jask1123@users.noreply.github.com>
2023-06-28 16:08:20 +0900

    変更

--- test/api/routers/task.py
+++ test/api/routers/task.py
@@ -11,7 +11,6 @@ async def list_tasks(db: Session = Depends(get_db)):
     return task_crud.get_tasks_with_done(db)
 
 
-
 @router.post('/tasks', response_model=task_schema.TaskCreateResponse)
 async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
     return task_crud.create_task(db, task_body)
