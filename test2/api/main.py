453a2aaf5c9cb3de4ad2f5f8a906e4cfc1cbb178
jask1123 <50744263+jask1123@users.noreply.github.com>
2023-07-21 16:24:28 +0900

    修正

--- test2/api/main.py
+++ test2/api/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 
-from api.routers import task,done,user,token
-
+from api.routers import task, done, user, token
 
 app = FastAPI()
-app.include_router(task.router)
-app.include_router(done.router)
-app.include_router(user.router)
-app.include_router(token.router)
 
-
-
+app.include_router(task.router, tags=["Task"])
+app.include_router(done.router, tags=["Done"])
+app.include_router(user.router, tags=["User"])
+app.include_router(token.router, tags=["Token"])
